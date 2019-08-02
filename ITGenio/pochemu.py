ebaeaf3a2d7a9e25d6a352542c2648fbf25e829b
Git-Sergio <44872284+Git-Sergio@users.noreply.github.com>
2019-08-02 18:43:43 +0200

    Update and rename pochemu.py to you_like_your_boss?.py

--- ITGenio/pochemu.py
+++
@@ -1,5 +0,0 @@
-kinder = ''
-while kinder != 'bo tak!':
-    kinder = input('chomu tak? \n')
-print('a, okej')
-input('Natysny "Enter" chob vyjty')
---
+++ ITGenio/you_like_your_boss?.py
@@ -0,0 +1,5 @@
+kinder = ''
+while kinder != 'I like my boss!':
+    kinder = input('you like your boss? \n')
+print('Okej')
+input('Thanks, Please touch "ENTER"')
