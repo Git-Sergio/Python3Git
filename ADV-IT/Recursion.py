a887978f0749cce0408e64c57491879805e08df6
Git-Sergio <sergii.grushkovyk@gmail.com>
2019-08-18 00:07:39 +0200

    Recursion

--- ADV-IT/Recursion.py
+++ ADV-IT/Recursion.py
@@ -1,3 +1,34 @@
+#Hello
+def hello(x):
+    if x == 0:
+        return
+    else:
+        print('Hello World!')
+        hello(x-1)
+
+hello(4)
+
+# Summ
+def factorial(x):
+    if x == 0:
+        return 1
+    else:
+        return x * factorial(x - 1)
+
+
+print(factorial(4))    #24
+
+
+def factorial(x):
+    if x == 0:
+        return 1
+    else:
+        return x * factorial(x-1)
+
+print(factorial(6))    #720
+
+
+#Fibonacci
 def fibo(x):
     if x == 0:
         return 0
@@ -7,4 +38,4 @@ def fibo(x):
         return fibo(x - 1) + fibo(x - 2)
 
 
-print(fibo(10))
+print(fibo(7))      #13
